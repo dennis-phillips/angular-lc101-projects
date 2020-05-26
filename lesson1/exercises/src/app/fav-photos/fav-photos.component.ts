2d7427487812790a6220f658335c956906c8da35
dennis-phillips <dennis_phillips@outlook.com>
2020-05-26 10:11:39 -0400

    new commit for angular lesson1 exercises part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Some Photos I Like';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.dropbox.com/s/viifa7bzjzxw71f/hiclipart.com.png?raw=1';
+  image3 = 'https://www.dropbox.com/s/viifa7bzjzxw71f/hiclipart.com.png?raw=1';
 
   constructor() { }
 
